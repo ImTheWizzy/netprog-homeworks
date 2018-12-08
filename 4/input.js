690e07d9777b238e743a766f5e5d829309488665
ImTheWizzy <martin_todo9@abv.bg>
2018-12-08 16:33:01 +0200

    git 'er done

---
+++ 4/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /[A-Z][a-z]+/,
+"exercise_2": /088[1-7]{7}/,
+"exercise_3": /[^0-1]+/,
+"exercise_4": /^[a-z][a-zA-Z0-9._]{2,13}$/,
+"exercise_5": /^[^2]+[^1]$/,
+"exercise_6": /class=['"](.*)['"]/
+};
+cssSelectors = {
+"exercise_1": "item > java",
+"exercise_2": "different java",
+"exercise_3": "java > tag",
+"exercise_4": "css > item:nth-child(3)",
+"exercise_5": "tag java:nth-child(2)",
+"exercise_6": "#someId > item > item > item > item",
+"exercise_7": "different > different:last-child > java:last-child",
+"exercise_8": "css > item:nth-child(2)"
+};
